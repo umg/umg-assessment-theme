9d2f6eb2212c41f53dd126cffdf52883f7957481
Travis Wagner <trvswgnr@gmail.com>
2023-11-08 21:35:33 -0500

    format

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,37 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'assets'),
-        filename: 'index.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1
-                        }
-                    },
-                    'postcss-loader'
-                ]
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
             },
-            {
-                test: /\.ts(x)?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [
-            '.tsx',
-            '.ts',
-            '.js'
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin()
-    ]
+          },
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.ts(x)?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
 
 module.exports = config;
